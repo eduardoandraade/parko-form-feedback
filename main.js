ff0289b7ea992b07949ffa69fb6af121857751f6
Eduardo Andrade <109284750+eduardoandraade@users.noreply.github.com>
2023-04-12 16:24:11 -0300

    inserindo starts no input radio

--- main.js
+++ main.js
@@ -17,8 +17,16 @@ inputs.forEach((input) => {
     input.addEventListener("blur", blurFunc)
 })
 
-const stars = document.querySelectorAll('.star-icon');
+const stars = document.querySelector('.star-icon');
                   
+function removeMouseListener() {
+  stars.forEach(function(star){
+    star.removeEventListener('mouseover', function(e){
+        e.stopPropagation();
+    });
+  });
+}
+
 document.getElementById('avaliacao').addEventListener('click', function(e){
   const classStar = e.target.classList;
   if(!classStar.contains('ativo')){
@@ -27,5 +35,7 @@ document.getElementById('avaliacao').addEventListener('click', function(e){
     });
     classStar.add('ativo');
     console.log(e.target.getAttribute('data-avaliacao'));
+    
+    removeMouseListener();
   }
 });
