41b0072a2c6812889c331b5e1db5e7ec773b5c15
Eduardo Andrade <109284750+eduardoandraade@users.noreply.github.com>
2023-04-18 19:04:19 -0300

    att js

--- main.js
+++ main.js
@@ -17,29 +17,58 @@ inputs.forEach((input) => {
     input.addEventListener("blur", blurFunc)
 })
 
-let stars = document.getElementById('.rating');
-
-
-                  
-function removeMouseListener() {
-  stars.forEach(function(star){
-    star.removeEventListener('mouseover', function(e){
-        e.stopPropagation();
+document.querySelector('.teste').addEventListener('submit', function(e) {
+    e.preventDefault();
+  
+    let formData = new FormData(this);
+  
+    // Itera sobre os dados de formulário
+    formData.forEach(function(value, key) {
+      localStorage.setItem(key, value);
     });
   });
+
+let stars = document.querySelectorAll(".ratings span");
+let products = document.querySelectorAll(".ratings");
+let ratings = [];
+
+for(let star of stars){
+    star.addEventListener("click", function(){
+        let children = star.parentElement.children;
+        for(let child of children){
+            if(child.getAttribute("data-clicked")){
+                return false;
+            }
+        }
+
+
+        this.setAttribute("data-clicked", "true")
+
+        let rating = this.dataset.rating;
+        let productId = this.parentElement.dataset.productid;
+        
+        let data = {
+            "stars": rating,
+            "product-id": productId
+        }
+
+        ratings.push(data);
+        localStorage.setItem("rating", JSON.stringify(ratings))
+    })
 }
 
-stars.addEventListener('click', function(e){
-  const classStar = e.target.classList;
-  if(!classStar.contains('ativo')){
-    stars.forEach(function(star){
-      star.classList.remove('ativo');
-    });
-    classStar.add('ativo');
-    
-    
-    removeMouseListener();
+if(localStorage.getItem("rating")){
 
-    console.log(e.target.getAttribute('data-avaliacao'));
-  }
-});
+    ratings = JSON.parse(localStorage.getItem("rating"))
+    for(let product of products){
+        if(ratings["product-id"] == product.dataset.productid){
+            let reversedStars = Array.from(product.children).reverse()
+            let index = parseInt(rating["stars"]) -1;
+
+            reversedStars[index].setAttribute("data-clicked", "true")
+        }
+    }
+}
+
+
+
