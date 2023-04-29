1098e65ea863569236decb8bb49f1609d3118a55
Eduardo Andrade <109284750+eduardoandraade@users.noreply.github.com>
2023-04-29 11:06:51 -0300

    update form e main
    Criando teste para db usando api json-serve e deixando o codigo com mais acessibilidade

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// Adicionando Estilo
+
 const inputs = document.querySelectorAll(".input");
 
 function focusFunc() {
@@ -17,19 +19,9 @@ inputs.forEach((input) => {
     input.addEventListener("blur", blurFunc)
 })
 
-document.querySelector('.teste').addEventListener('submit', function(e) {
-    e.preventDefault();
-  
-    let formData = new FormData(this);
-  
-    // Itera sobre os dados de formulário
-    formData.forEach(function(value, key) {
-      localStorage.setItem(key, value);
-    });
-  });
+// Adicionando Avaliação usando estrelas 
 
 let stars = document.querySelectorAll(".ratings span");
-let products = document.querySelectorAll(".ratings");
 let ratings = [];
 
 for(let star of stars){
@@ -43,29 +35,50 @@ for(let star of stars){
                 this.setAttribute("data-clicked", "true");
             }
         }
-
-        let rating = this.dataset.rating;
-        let productId = this.parentElement.dataset.productid;
-        
-        let data = {
-            "stars": rating,
-            "product-id": productId
-        }
-
-        ratings.push(data);
-        localStorage.setItem("rating", JSON.stringify(ratings))
     })
+
+// Recuperando value do span clicado
+
+    const ratingDiv = document.querySelector('.ratings');
+    const ratingInput = document.querySelector('#ratingSelected')
+
+    const ratingStars = ratingDiv.querySelectorAll('span')
+    ratingStars.forEach((star) => {
+    star.addEventListener('click', () => {
+        const ratingValue = star.getAttribute('data-rating')
+
+        ratingInput.value = ratingValue;
+    })
+})
 }
 
-if(localStorage.getItem("rating")){
+// Enviando dados do formulário
 
-    ratings = JSON.parse(localStorage.getItem("rating"))
-    for(let product of products){
-        if(ratings["product-id"] == product.dataset.productid){
-            let reversedStars = Array.from(product.children).reverse()
-            let index = parseInt(rating["stars"]) -1;
+const form = document.getElementById('feedback-form')
+const ratingSelected = document.getElementById('ratingSelected')
 
-            reversedStars[index].setAttribute("data-clicked", "true")
-        }
-    }
-}
+form.addEventListener('submit', (ev) => {
+    ev.preventDefault();
+
+    const formData = new FormData(form);
+    const feedback = {
+        name: formData.get('name'),
+        email: formData.get('email'),
+        rating: formData.get('rating'),
+        message: formData.get('message')
+    };
+
+    fetch('http://localhost:3000/feedback', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(feedback)
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error))
+
+    form.reset()
+})
+
